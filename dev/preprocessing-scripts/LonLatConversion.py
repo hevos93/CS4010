6bcd4262fb24e235e517c083fe15d3b91d26d452
siko2222 <sinols2000@hotmail.no>
2023-09-14 17:20:07 +0200

    Cosmetic changes to code

--- dev/preprocessing-scripts/LonLatConversion.py
+++ dev/preprocessing-scripts/LonLatConversion.py
@@ -1,6 +1,8 @@
+# Importing libraries
 import pandas as pd
 from pyproj import Transformer, CRS
 
+# Function to convert EPSG:5972 to EPSG:4326 in decimal degrees
 def convert_gps(lon, lat):
     # Define the source (EPSG:5973) and target (EPSG:4326) projections
     src_projection = CRS.from_string("EPSG:5973")
@@ -11,28 +13,29 @@ def convert_gps(lon, lat):
     converted_lon, converted_lat = transformer.transform(lon, lat)
     return converted_lon, converted_lat
 
-
+# Reading the csv file
 filename = '../datasets/accidents.csv'
-
 df = pd.read_csv(filename)
 
+# Create lists from dataframe lat, lon columns.
 lon_list = df['Lon'].tolist()
 lat_list = df['Lat'].tolist()
 
-counter = 0
-
+# Create empty lists for converted lat, lon
 lon_list_converted = []
 lat_list_converted = []
 
+# Assuming lon_list and lat_list of equall length, loop through and convert every coordinate pair
+counter = 0
 while counter < len(lon_list):
     result = convert_gps(lon_list[counter], lat_list[counter])
     lon_list_converted.append(result[1]) 
     lat_list_converted.append(result[0]) 
     counter = counter + 1
 
+# Turn the converted lists back into columns in the dataframe
 df['Lon'] = pd.Series(lon_list_converted)
 df['Lat'] = pd.Series(lat_list_converted)
 
 # Save to csv
 df.to_csv(filename, index=False, quoting=1)
-print(df.head(5))
--- dev/preprocessing-scripts/sparkPreproccessSpeedLimit.py
+++ dev/preprocessing-scripts/sparkPreproccessSpeedLimit.py
@@ -1,3 +1,4 @@
+# Importing libraries
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
